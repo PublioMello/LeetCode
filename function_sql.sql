a94defd4213eb28cb2d870179f23a1a934208579
Publio Mello <publio.lima.mello@gmail.com>
2025-07-30 19:39:01 -0300

    another funtcion

--- function_sql.sql
+++ function_sql.sql
@@ -16,3 +16,32 @@ end $$;
 select analise_estoque(10,50);
 
 select count (*) - analise_estoque(10,50) from products;
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+create or replace function busca_clientes(title varchar)
+returns table
+		(
+			id customers.customer_id%type,
+			nome customers.contact_name%type,
+			telefone customers.phone%type,
+			cargo customers.contact_title%type
+		)
+
+language plpgsql
+as $$
+
+begin
+	return query
+		select
+			customer_id,
+			contact_name,
+			phone,
+			contact_title
+		from customers
+		where contact_title = title;
+end $$;
+
+select * from busca_clientes('Owner')
