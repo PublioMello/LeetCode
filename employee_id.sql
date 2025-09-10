4891b77c56706962989821b3d56c65e236228dc1
PublioMello <publio.lima.mello@gmail.com>
2025-09-10 19:20:47 -0300

    DD

---
+++ employee_id.sql
@@ -0,0 +1,16 @@
+-- For this problem, we will consider a manager an employee who has at least 1 other employee reporting to them.
+
+-- Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
+
+-- Return the result table ordered by employee_id.
+
+
+select
+    rt.employee_id,
+    rt.name,
+    count(rt.employee_id) as reports_count,
+    round(avg(e.age)) as average_age
+FROM Employees e
+join Employees rt on e.reports_to = rt.employee_id
+group by rt.employee_id,rt.name
+order by rt.employee_id
